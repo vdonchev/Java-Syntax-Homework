bc8dbf22d25c21aac901d055913d5253ee3eb54f
vdonchev <ya.5164646777752015@yandex.ru>
2015-10-15 22:19:22 +0300

    Update MagicExchangeableWords.java
    Fix suggested by enevlogiev

--- src/MagicExchangeableWords.java
+++ src/MagicExchangeableWords.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 public class MagicExchangeableWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Insert 2 equal by length strings, separated by space = ");
-        System.out.println();
-        System.out.println(findIfWordsAreMagic(scanner.next(), scanner.next()));
+
+        String stringA = scanner.next();
+        String stringB = scanner.next();
+        System.out.println(findIfWordsAreMagic(stringA, stringB));
     }
 
     private static boolean findIfWordsAreMagic(String strA, String strB) {
-        boolean equal = true;
         Map<Character, Character> archive = new HashMap<>();
+
         for (int i = 0; i < strA.length(); i++) {
-            if(archive.containsKey(strA.charAt(i))) {
-                if(archive.get(strA.charAt(i)) != strB.charAt(i)) {
-                    equal = false;
+            char charA = strA.charAt(i);
+            char charB = strB.charAt(i);
+
+            if (archive.containsKey(charA)) {
+                if (archive.get(charA) != charB) {
+                    return false;
                 }
             } else {
-                archive.put(strA.charAt(i), strB.charAt(i));
+                if (archive.containsValue(charB)) {
+                    return false;
+                }
+
+                archive.put(charA, charB);
             }
         }
 
-        return equal;
+        return true;
     }
 }
